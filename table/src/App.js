27bb50523a13e1cc737239ee810e3c76144d3ecb
tranducanh <caocunjcyc@gmail.com>
2021-01-02 02:35:43 +0700

    update
    update

--- table/src/App.js
+++ table/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Edit} from './Edit';
+import "./App.css";
+import { Table } from "./Table";
+import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/Table">Table</Link>
+      <Route
+        path="/Table"
+        render={() => <Table></Table>}
+      />
+      <Route path="/edit/:id" component={Edit} />
     </div>
   );
 }
